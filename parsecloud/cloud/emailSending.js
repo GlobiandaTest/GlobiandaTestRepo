85f08bd50548b1a23ac5f7b30766397dda2fec14
sergiojara@globant.com <3783299276a>
2015-04-07 16:23:48 -0300

    tu pendex

---
+++ parsecloud/cloud/emailSending.js
@@ -0,0 +1,66 @@
+
+var Mandrill = require('mandrill');
+Mandrill.initialize('vHa8A1DV1EfoY7INdZATbQ');
+
+
+Mandrill.sendEmail({
+  message: {
+    text: "Hello World!",
+    subject: "Using Cloud Code and Mandrill is great!",
+    from_email: "parse@cloudcode.com",
+    from_name: "Cloud Code",
+    to: [
+      {
+        email: "jarasergio@outlook.com",
+        name: "Sergio jara"
+      }
+    ]
+  },
+  async: true
+},{
+  success: function(httpResponse) {
+    console.log(httpResponse);
+    response.success("Email sent!");
+  },
+  error: function(httpResponse) {
+    console.error(httpResponse);
+    response.error("Uh oh, something went wrong");
+  }
+});
+
+
+var sendEmail = function(toAddress, name)
+{
+	
+var Mandrill = require('mandrill');
+Mandrill.initialize('vHa8A1DV1EfoY7INdZATbQ');
+
+
+Mandrill.sendEmail({
+  message: {
+    text: "Hello World!",
+    subject: "Using Cloud Code and Mandrill is great!",
+    from_email: "parse@cloudcode.com",
+    from_name: "Cloud Code",
+    to: [
+      {
+        email: toAddress,
+        name: name
+      }
+    ]
+  },
+  async: true
+},{
+  success: function(httpResponse) {
+    console.log(httpResponse);
+    response.success("Email sent!");
+  },
+  error: function(httpResponse) {
+    console.error(httpResponse);
+    response.error("Uh oh, something went wrong");
+  }
+});
+
+
+
+};
